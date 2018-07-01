d15ab94699761b1c5fb9d9b1caf95503f6b651e6
Vadim Gorbenko <gamerlands@mail.ru>
2018-07-01 18:28:05 +0300

    Добавлена заглушка для AddForm.onSubmit и установлен паке react-redux

--- src/components/AddForm/index.jsx
+++ src/components/AddForm/index.jsx
@@ -3,18 +3,22 @@ import React, {Component} from 'react';
 export default class AddForm extends Component{
 	render(){
 		return(
-			<form>
+			<form onSubmit = {this.isValid.bind(this)}>
 	      <label>Фамилия
-	        <input type="text" value="Фамилия" placeholder="Фамилия"/>
+	        <input type="text" ref="lastName" defaultValue="Фамилия" placeholder="Фамилия"/>
 	      </label>
 	      <label>Имя
-	        <input type="text" value="Имя" placeholder="Имя"/>
+	        <input type="text" ref="firstName" defaultValue="Имя" placeholder="Имя"/>
 	      </label>
 	      <label>Отчество
-	        <input type="text" value="Отчество" placeholder="Отчество"/>
+	        <input type="text" ref="middleName" defaultValue="Отчество" placeholder="Отчество"/>
 	      </label>
-	      <input type="submit" value="Добавить"/>
+	      <input type="submit" defaultValue="Добавить"/>
    		</form>
 		)
+	}
+	isValid = (event) => {
+		event.preventDefault()
+		console.log(this)
 	}
 }
