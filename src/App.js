fcfbae62e19e6872ed92c0ba74c7bf1a5b2a7c9b
Vadim Gorbenko <gamerlands@mail.ru>
2018-07-01 17:23:16 +0300

    Добавлен и выведен компонент TableList
    таблица со списком ФИО и кнопками редактировать\удалить

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import AddForm from "./components/AddForm/"
+import TableList from "./components/TableList/"
 
 export default class App extends Component{
 	render(){
 		return(
 			<div>
 				<AddForm/>
+				<TableList/>
 			</div>
 		)
 	}
