754dcf00c91adc93dbdd865a013614f4ed7f39e2
Vadim Gorbenko <gamerlands@mail.ru>
2018-07-01 20:44:44 +0300

    Подключение Redux DevTools
    Код проверяет становлено ли расширение Redux DevTools и если да, то начинает работать с ним.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import combinedReducers from './reducers/'
 
-const store = createStore(combinedReducers)
+const store = createStore(combinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
 	<Provider store={store}>
