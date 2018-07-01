d80ab5e73761ef0f0783ba605b231d6fd5392d41
Vadim Gorbenko <gamerlands@mail.ru>
2018-07-01 19:58:53 +0300

    Создан reducer listReducer и хранилище и функция для отображения данных в таблице.
    listReducer хранит в себе массив с ФИО. reducers/Index.js - комбинированный reducer. store - Хранилище. Provider Для соединения хранилища и компонентов.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+import combinedReducers from './reducers/'
 
-ReactDOM.render(<App />, document.getElementsByClassName('main')[0]);
+const store = createStore(combinedReducers)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,document.getElementsByClassName('main')[0]);
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+import listReducer from './listReducer'
+
+const combinedReducers = combineReducers({
+	list: listReducer
+})
+
+export default combinedReducers
